61e41da45510e2f9089417d051111081a90508ff
chandrakant sir <cakant70@gmail.com>
2023-01-18 10:28:22 +0530

    project upload

---
+++ src/MyServlet1.java
@@ -0,0 +1,26 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+
+public class MyServlet1 extends HttpServlet{
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		resp.setContentType("text/html");
+		PrintWriter out=resp.getWriter();
+		out.print("welcome to <b>servlet</b> day 2<br/>");
+		
+	}
+
+	
+	
+	
+}
